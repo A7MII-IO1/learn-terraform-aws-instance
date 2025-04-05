de55073e1be47f85724bca853452092b7ed67985
a7mii-io1 <a7mii.io1@gmail.com>
2025-04-06 06:22:22 +0700

    Initial commit for Terraform setup

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "learn-hashicorp-remote"
+    workspaces {
+      name = "learn-terraform-aws"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c1907b6d738188e5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
